26b1298335b1143eb33342fe1bba2a5c5ada548c
yuhonghai123 <1183569689@qq.com>
2022-02-17 14:46:08 +0800

    feat: Echarts fabric

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import fabric from 'fabric'
+import 'echarts-fabric'
+import * as echarts from 'echarts'
 Vue.config.productionTip = false
+
+Vue.prototype.$echarts = echarts;
+Vue.use(fabric);
 
 new Vue({
   render: h => h(App),
